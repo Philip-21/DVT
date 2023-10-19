b6cd6c25f47587f7b00fdc692f617022cdd30f85
Philip-21 <philipuzomaobiora@gmail.com>
2023-10-19 14:22:08 +0100

    the export package to be used
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

---
+++ dvl/export.go
@@ -0,0 +1,12 @@
+package dvl
+
+import "github.com/philip21/datavalidator/validate"
+
+var (
+	ValidateEmail         = validate.ValidateEmail
+	ValidateEmailtoBytes  = validate.ValidateEmailtoBytes
+	ValidateEmailToString = validate.ValidateEmailToString
+	ValidatePhone         = validate.ValidatePhone
+	ValidatePhoneToBytes  = validate.ValidatePhoneToBytes
+	ValidatePhoneToString = validate.ValidatePhoneToString
+)
