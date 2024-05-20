e4a81fd121d71c0e6a7199d9ed22bce63de6f9f2
Philip-21 <philipuzomaobiora@gmail.com>
2024-05-20 19:40:45 +0100

    refactoring codes for modularity
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

--- transform/json/time.go
+++ transform/json/time.go
@@ -1,99 +1,20 @@
 package json
 
 import (
-	"encoding/json"
-	"fmt"
-	"regexp"
+	"github.com/philip21/dvt/transform"
 )
 
-func ErrTime() string {
-	errt :=
-		`
-	Ensure you have the right time stamp format [GMT, WAT, CET, EET, CST]
-	XXXX-XX-XXTXX:XX:XX+00:00 GMT,
-	XXXX-XX-XXTXX:XX:XX+01:00 WAT,
-	XXXX-XX-XXTXX:XX:XX+01:00 CET,
-	XXXX-XX-XXTXX:XX:XX+02:00 EET,
-	XXXX-XX-XXTXX:XX:XX-06:00 CST,
-	`
-	return errt
-}
+// initialize default instance of TransformDateTime{},to be made available for package level
+var defaultTransform = transform.NewDateTimeTransform()
 
-var Errtime = ErrTime()
-
-
-func DateTimeToString(timeStamp string) (string, error) {
-	gmtTimeStamp := `^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+00:00$` //"2023-07-10T16:22:41+00:00"
-	watTimeStamp := `^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+01:00$` //"2023-07-10T16:22:41+01:00"
-	cetTimeStamp := `^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+01:00$` //"2023-07-10T16:22:41+01:00"
-	eetTimeStamp := `^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+02:00$` //"2023-07-10T16:22:41+02:00"
-	cstTimeStamp := `^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\-06:00$` //"2023-07-10T16:22:41-06:00"
-
-	var regEXP *regexp.Regexp
-
-	switch {
-	case regexp.MustCompile(gmtTimeStamp).MatchString(timeStamp):
-		regEXP = regexp.MustCompile(gmtTimeStamp)
-
-	case regexp.MustCompile(watTimeStamp).MatchString(timeStamp):
-		regEXP = regexp.MustCompile(watTimeStamp)
-
-	case regexp.MustCompile(cetTimeStamp).MatchString(timeStamp):
-		regEXP = regexp.MustCompile(cetTimeStamp)
-
-	case regexp.MustCompile(eetTimeStamp).MatchString(timeStamp):
-		regEXP = regexp.MustCompile(eetTimeStamp)
-
-	case regexp.MustCompile(cstTimeStamp).MatchString(timeStamp):
-		regEXP = regexp.MustCompile(cstTimeStamp)
-	default:
-		return "", fmt.Errorf("invalid DateTime format: %s", timeStamp)
-	}
-
-	if !regEXP.MatchString(timeStamp) {
-		return "", fmt.Errorf("invalid timestamp format for %s", timeStamp)
-	}
-	jsonString, err := json.Marshal(timeStamp)
-	if err != nil {
-		return "", err
-	}
-	return string(jsonString), nil
-}
-
+// validates a datetime to a []byte type , this give more flexibility,
+// allowing you to work with the JSON data in its raw binary form, using []byte
+// approach provides more control over how the JSON is handled
 func DateTimeToBytes(timeStamp string) ([]byte, error) {
-	gmtTimeStamp := `^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+00:00$` //"2023-07-10T16:22:41+00:00"
-	watTimeStamp := `^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+01:00$` //"2023-07-10T16:22:41+01:00"
-	cetTimeStamp := `^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+01:00$` //"2023-07-10T16:22:41+01:00"
-	eetTimeStamp := `^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+02:00$` //"2023-07-10T16:22:41+02:00"
-	cstTimeStamp := `^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\-06:00$` //"2023-07-10T16:22:41-06:00"
+	return defaultTransform.DateTimeToBytes(timeStamp)
+}
 
-	var regEXP *regexp.Regexp
-
-	switch {
-	case regexp.MustCompile(gmtTimeStamp).MatchString(timeStamp):
-		regEXP = regexp.MustCompile(gmtTimeStamp)
-
-	case regexp.MustCompile(watTimeStamp).MatchString(timeStamp):
-		regEXP = regexp.MustCompile(watTimeStamp)
-
-	case regexp.MustCompile(cetTimeStamp).MatchString(timeStamp):
-		regEXP = regexp.MustCompile(cetTimeStamp)
-
-	case regexp.MustCompile(eetTimeStamp).MatchString(timeStamp):
-		regEXP = regexp.MustCompile(eetTimeStamp)
-
-	case regexp.MustCompile(cstTimeStamp).MatchString(timeStamp):
-		regEXP = regexp.MustCompile(cstTimeStamp)
-	default:
-		return nil, fmt.Errorf("invalid DateTime format: %s", timeStamp)
-	}
-
-	if !regEXP.MatchString(timeStamp) {
-		return nil, fmt.Errorf("invalid timestamp format for %s", timeStamp)
-	}
-	jsonString, err := json.Marshal(timeStamp)
-	if err != nil {
-		return nil, err
-	}
-	return jsonString, nil
+// validate's and converts datetime to a string JSON object.
+func DateTimeToString(timeStamp string) (string, error) {
+	return defaultTransform.DateTimeToString(timeStamp)
 }
