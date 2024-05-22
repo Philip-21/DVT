d62f95371c7ee115ac773ba1e16d959a648a30a2
Philip-21 <philipuzomaobiora@gmail.com>
2024-05-22 18:39:06 +0100

    new transform phone functions
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

--- transform/transform.go
+++ transform/transform.go
@@ -10,6 +10,10 @@ type TransformDateTime struct {
 	Regexps map[string]*regexp.Regexp
 }
 
+type TransformPhone struct {
+	Countrycode map[string]*regexp.Regexp
+}
+
 // NewDateTimeTransform Initialize the  TransformDateTime struct
 func NewDateTimeTransform() *TransformDateTime {
 	return &TransformDateTime{
@@ -21,6 +25,84 @@ func NewDateTimeTransform() *TransformDateTime {
 			"cst": regexp.MustCompile(`^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\-06:00$`), // "2023-07-10T16:22:41-06:00"
 		},
 	}
+}
+
+// NewValidPhone initializes the TransformPhone struct
+func NewValidPhone() *TransformPhone {
+	countryCodeNGA := `^\+234-\d{10}$`
+	countryCodeUSA := `^\+1-\d{10}$`
+	countryCodeGER := `^\+44-\d{10}$`
+	countryCodeENG := `\+49-\d{10}$`
+	countryCodeIND := `\+91-\d{10}$`
+	return &TransformPhone{
+		Countrycode: map[string]*regexp.Regexp{
+			"+234": regexp.MustCompile(countryCodeNGA),
+			"+1":   regexp.MustCompile(countryCodeUSA),
+			"+44":  regexp.MustCompile(countryCodeENG),
+			"+49":  regexp.MustCompile(countryCodeGER),
+			"+91":  regexp.MustCompile(countryCodeIND),
+		},
+	}
+}
+
+// validate phone number based on country code
+// NGA, USA, ENG, GER, IND
+// returns  a JSON string object
+func (t *TransformPhone) PhoneToString(phone any) (string, error) {
+	for _, re := range t.Countrycode {
+		phonestr, ok := phone.(string)
+		if !ok {
+			return "", fmt.Errorf("unable to convert to string")
+		}
+		if len(phonestr) < 10 || phonestr[0] != '+' {
+			return "", fmt.Errorf("invalid phonestr field format, ensure you have + added at the beginning of the phone number")
+		}
+		if len(phonestr) < 10 || phonestr[0] != '+' {
+			return "", fmt.Errorf("invalid phonestr field format, ensure you have + added at the beginning of the phone number")
+		}
+		if re.MatchString(phonestr) {
+			jsonBytes, err := json.Marshal(phonestr)
+			if err != nil {
+				return "", err
+			}
+			jsonString := string(jsonBytes)
+			return jsonString, nil
+		}
+	}
+	phonestr, ok := phone.(string)
+	if !ok {
+		return "", fmt.Errorf("unable to convert to string")
+	}
+	return "", fmt.Errorf("invalid country code: %s, valid country codes are +234, +1,+44, +49, +91", phonestr)
+}
+
+// validate phone number based on country code
+// NGA, USA, ENG, GER, IND
+// to a []byte, this give more flexibility,
+// allowing you to work with the JSON data in its raw binary form, using []byte
+// approach provides more control over how the JSON is handled
+func (t *TransformPhone) PhoneToBytes(phone any) ([]byte, error) {
+	for _, re := range t.Countrycode {
+		phonestr, ok := phone.(string)
+		if !ok {
+			return nil, fmt.Errorf("unable to convert to string")
+		}
+		if len(phonestr) < 10 || phonestr[0] != '+' {
+			return nil, fmt.Errorf("invalid phonestr field format, ensure you have + added at the beginning of the phone number")
+		}
+		if len(phonestr) < 10 || phonestr[0] != '+' {
+			return nil, fmt.Errorf("invalid phonestr field format, ensure you have + added at the beginning of the phone number")
+		}
+		if re.MatchString(phonestr) {
+			jsonBytes, err := json.Marshal(phonestr)
+			if err != nil {
+				return nil, err
+			}
+			return jsonBytes, nil
+		}
+	}
+	return nil, fmt.Errorf("invalid country code: %s, valid country codes are +234, +1,+44, +49, +91", phone)
+
 }
 
 // validates a datetime to a []byte type , this give more flexibility,
