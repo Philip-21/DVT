43fc70a4e630f1c198d9200d5a4984eae183b8e4
Philip-21 <philipuzomaobiora@gmail.com>
2024-05-22 18:39:59 +0100

    new phone functions
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

--- validate/validate.go
+++ validate/validate.go
@@ -5,11 +5,13 @@ import (
 	"regexp"
 )
 
+type ValidPhone struct {
+	Countrycode map[string]*regexp.Regexp
+}
+
 type ValidDateTime struct {
 	Regexps map[string]*regexp.Regexp
 }
-
-var defaultTransform = NewDateTimeValidate()
 
 // NewDateTimeValidate Initializes the  ValidDateTime struct
 func NewDateTimeValidate() *ValidDateTime {
@@ -25,12 +27,56 @@ func NewDateTimeValidate() *ValidDateTime {
 }
 
 // validate timezone and returns the validated timezone and a bool value
-func (t *ValidDateTime) ValidateDateTime(timestamp string) (any, bool, error) {
-	for _, re := range t.Regexps {
+func (v *ValidDateTime) ValidateDateTime(timestamp string) (any, bool, error) {
+	for _, re := range v.Regexps {
 		if re.MatchString(timestamp) {
 			return timestamp, true, nil
 		}
 
 	}
 	return timestamp, false, fmt.Errorf("invalid datetime format: %s", timestamp)
+}
+
+// NewValidPhone initializes the ValidPhone struct
+func NewValidPhone() *ValidPhone {
+	countryCodeNGA := `^\+234-\d{10}$`
+	countryCodeUSA := `^\+1-\d{10}$`
+	countryCodeGER := `^\+44-\d{10}$`
+	countryCodeENG := `\+49-\d{10}$`
+	countryCodeIND := `\+91-\d{10}$`
+	return &ValidPhone{
+		Countrycode: map[string]*regexp.Regexp{
+			"+234": regexp.MustCompile(countryCodeNGA),
+			"+1":   regexp.MustCompile(countryCodeUSA),
+			"+44":  regexp.MustCompile(countryCodeENG),
+			"+49":  regexp.MustCompile(countryCodeGER),
+			"+91":  regexp.MustCompile(countryCodeIND),
+		},
+	}
+}
+
+// validate phone number based on country code
+// NGA, USA, ENG, GER, IND
+// returns the validated Phone and a bool value
+func (v *ValidPhone) ValidPhone(phone any) (any, bool, error) {
+	for _, re := range v.Countrycode {
+		phonestr, ok := phone.(string)
+		if !ok {
+			return nil, false, fmt.Errorf("unable to convert to string")
+		}
+		if len(phonestr) < 10 || phonestr[0] != '+' {
+			return nil, false, fmt.Errorf("invalid phonestr field format, ensure you have + added at the beginning of the phone number")
+		}
+		if len(phonestr) < 10 || phonestr[0] != '+' {
+			return nil, false, fmt.Errorf("invalid phonestr field format, ensure you have + added at the beginning of the phone number")
+		}
+		if re.MatchString(phonestr) {
+			return phonestr, true, nil
+		}
+	}
+	phonestr, ok := phone.(string)
+	if !ok {
+		return nil, false, fmt.Errorf("unable to convert to string")
+	}
+	return nil, false, fmt.Errorf("invalid country code: %s, valid country codes are +234, +1,+44, +49, +91", phonestr)
 }
